bd6eca3160051fc5c57873200777d3d0b9033def
carlpeters <cpjjpeters@gmail.com>
2022-06-09 15:25:41 +0200

    first commit

---
+++ src/main/java/be/ipeters/unti/UnitApplication.java
@@ -0,0 +1,21 @@
+package be.ipeters.unti;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@Slf4j
+public class UnitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UnitApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
